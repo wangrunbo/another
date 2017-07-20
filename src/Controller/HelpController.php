ab3b94dfa1e92c8ef0fb1947a6541ff197622198
wangrunbo <wangrunbo921@gmail.com>
2017-07-20 14:10:17 +0900

    基本页面

--- src/Controller/HelpController.php
+++ src/Controller/HelpController.php
@@ -6,11 +6,10 @@ use App\Controller\AppController;
 /**
  * Help Controller
  *
- * @property \App\Model\Table\HelpTable $Help
  */
 class HelpController extends AppController
 {
-    
+
     public function index()
     {
 
